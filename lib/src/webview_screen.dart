ae03e7c79fa8a5abac46f876838967a6fe6b5771
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-03-13 10:50:04 +0300

    new-pubspec-changes

--- lib/src/webview_screen.dart
+++ lib/src/webview_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 import 'dart:io';
-
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:flutter/material.dart';
 import 'package:hesabe/hesabe.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -24,58 +26,47 @@ class _WebviewScreenState extends State<WebviewScreen> {
   bool flag = false;
   late final WebViewController _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-  }
+@override
+void initState() {
+  super.initState();
+
+  _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onPageFinished: (String url) {
+          if (widget.responseUrl != null && url.contains('${widget.responseUrl}')) {
+            if (mounted) setState(() { flag = true; });
+            parseResult(url);
+          } else if (widget.failureUrl != null && url.contains('${widget.failureUrl}')) {
+            if (mounted) setState(() { flag = true; });
+            parseResult(url);
+          }
+        },
+        onPageStarted: (url) {
+          log('onPageStarted $url');
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse(widget.paymentURL));
+}
+
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: WillPopScope(
-        onWillPop: () async {
-          Navigator.of(context).pop(Hesabe.EVENT_PAYMENT_CANCELLED_BY_USER);
-          return false;
-        },
-        child: Scaffold(
-          body: WebView(
-            initialUrl: widget.paymentURL,
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller = webViewController;
-              if (!flag) {
-                _controller.loadUrl(widget.paymentURL);
-              }
-            },
-            onPageFinished: (String url) {
-              /* Check if URL contains the value given at 'responseUrl' field */
-              if (widget.responseUrl != null &&
-                  url.contains('${widget.responseUrl}')) {
-                if (mounted)
-                  setState(() {
-                    flag = true;
-                  });
-                /* If yes, parse the result */
-                parseResult(url);
-              } else if (widget.failureUrl != null &&
-                  url.contains('${widget.failureUrl}')) {
-                if (mounted)
-                  setState(() {
-                    flag = true;
-                  });
-                parseResult(url);
-              }
-            },
-            onPageStarted: (url) {
-              log('onPageStarted $url');
-            },
-            gestureNavigationEnabled: true,
-          ),
-        ),
+ Widget build(BuildContext context) {
+  return SafeArea(
+    child: WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).pop(Hesabe.EVENT_PAYMENT_CANCELLED_BY_USER);
+        return false;
+      },
+      child: Scaffold(
+        body: WebViewWidget(controller: _controller), // ✅ Use WebViewWidget
       ),
-    );
-  }
+    ),
+  );
+}
+
 
   void parseResult(String url) {
     final parse = Uri.parse(url);
